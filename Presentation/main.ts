aa6bf8398d7fe12313380adcf9abc4ffa8cac76f
Stephen Tillman <askmrtillman@gmail.com>
2021-02-27 23:31:57 -0500

    install + configure file upload; ignore uploads folder

--- Presentation/main.ts
+++ Presentation/main.ts
@@ -4,15 +4,20 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { GravatarModule } from './gravatar/gravatar.module';
 import { MainModule } from './main.module';
+const fileUpload = require('fastify-file-upload')
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     MainModule,
     new FastifyAdapter(),
   );
-
+  app.register(fileUpload, {
+    limits: { fileSize: 1 * 1024 * 1024 },
+    abortOnLimit: true,
+    responseOnLimit: 'The file is too large. Please select a file less than 1MB.',
+    createParentPath: true,
+  });
   app.useGlobalFilters(new UnauthorizedErrorFilter());
   app.enableCors();
 
