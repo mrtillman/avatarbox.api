9d45a9f7feb96b8aaf5421450f7f1be759d57035
Stephen Tillman <askmrtillman@gmail.com>
2021-03-01 18:57:19 -0500

    implement collect, dig, & peek methods

--- Presentation/main.controller.ts
+++ Presentation/main.controller.ts
@@ -1,9 +1,35 @@
 import { Controller, Get, Req } from '@nestjs/common';
+import { AvbxGravatarClient } from 'avatarbox.sdk';
 import { Request } from 'express';
+
+export const route = {
+  collect: 'collect',
+  dig: 'dig',
+  peek: 'peek'
+};
 
 @Controller()
 export class MainController {
-  constructor() {}
+  public client: AvbxGravatarClient;
+
+  constructor() {
+    this.client = new AvbxGravatarClient();
+  }
+
+  @Get('collect')
+  async collect(@Req() req: Request): Promise<any> {
+    return await this.client.collect();
+  }
+
+  @Get('dig')
+  async dig(@Req() req: Request): Promise<any> {
+    return await this.client.dig();
+  }
+
+  @Get('peek')
+  async peek(@Req() req: Request): Promise<any> {
+    return await this.client.peek();
+  }
 
   @Get('/')
   getHome(@Req() req: Request): string {
--- Presentation/main.module.ts
+++ Presentation/main.module.ts
@@ -1,11 +1,24 @@
-import { MiddlewareConsumer } from '@nestjs/common';
+import { MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { Module, NestModule } from '@nestjs/common';
 import { MainController } from './main.controller';
 import { GravatarModule } from './gravatar/gravatar.module';
+import { JwtMiddleware } from './jwt.middleware';
+import { route } from './main.controller';
 
 @Module({
   imports: [GravatarModule],
   controllers: [MainController],
   providers: [],
 })
-export class MainModule {}
+export class MainModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    const routes = [
+      { path: route.collect, method: RequestMethod.GET },
+      { path: route.dig, method: RequestMethod.GET },
+      { path: route.peek, method: RequestMethod.GET },
+    ];
+    consumer
+      .apply(JwtMiddleware)
+      .forRoutes(...routes)
+  }
+}
