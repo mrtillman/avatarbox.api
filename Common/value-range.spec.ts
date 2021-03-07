67a685990ff27e8df106974757e9fcd120e7339c
Stephen Tillman <askmrtillman@gmail.com>
2021-03-06 19:38:07 -0500

    revise unit test stub

--- Common/value-range.spec.ts
+++ Common/value-range.spec.ts
@@ -2,10 +2,11 @@ import { ValueRange } from './value-range';
 
 describe('ValueRange', () => {
   let valueRange: ValueRange;
-
   describe('root', () => {
-    it('should work', () => {
-      expect(true).toBe(true);
+    it('should default to min', () => {
+      const min = 0, max = 3;
+      valueRange = new ValueRange(min,max);
+      expect(valueRange.value).toBe(min);
     });
   });
 });
--- Common/value-range.ts
+++ Common/value-range.ts
@@ -1,6 +1,6 @@
 export class ValueRange {
   public value: any;
-  constructor(min: any, max: any, value: any) {
+  constructor(min: any, max: any, value: any = null) {
     this.value = value || min;
     if (value < min || value > max) {
       this.value = min;
