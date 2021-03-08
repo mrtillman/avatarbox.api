be313a59795f0f662130ef7b29bd128a502e8624
Stephen Tillman <askmrtillman@gmail.com>
2021-03-07 19:46:41 -0500

    finalize value range tests

--- Common/value-range.spec.ts
+++ Common/value-range.spec.ts
@@ -4,9 +4,31 @@ describe('ValueRange', () => {
   let valueRange: ValueRange;
   describe('root', () => {
     it('should default to min', () => {
-      const min = 0, max = 3;
-      valueRange = new ValueRange(min,max);
+      const min = 0;
+      const max = 3;
+      valueRange = new ValueRange(min, max);
       expect(valueRange.value).toBe(min);
+    });
+    it('should default to min when value too large', () => {
+      const min = 0;
+      const max = 3;
+      const value = 5;
+      valueRange = new ValueRange(min, max, value);
+      expect(valueRange.value).toBe(min);
+    });
+    it('should default to min when value too small', () => {
+      const min = 0;
+      const max = 3;
+      const value = -5;
+      valueRange = new ValueRange(min, max, value);
+      expect(valueRange.value).toBe(min);
+    });
+    it('should set value when value is within range', () => {
+      const min = 0;
+      const max = 3;
+      const value = 2;
+      valueRange = new ValueRange(min, max, value);
+      expect(valueRange.value).toBe(value);
     });
   });
 });
